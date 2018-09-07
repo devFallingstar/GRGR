a5ca5e1339dd3a97febab04bb1be3d89d2265641
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-09-07 13:02:59 +0900

    Add extensions

--- TextAndPic/Core/AppDelegate.swift
+++ TextAndPic/Core/AppDelegate.swift
@@ -58,7 +58,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
         
         return false
     }
-    
-    
+}
+
+extension String {
+    func fromBase64() -> String? {
+        guard let data = Data(base64Encoded: self) else {
+            return nil
+        }
+        return String(data: data, encoding: .utf8)
+    }
+    func toBase64() -> String {
+        return Data(self.utf8).base64EncodedString()
+    }
+}
+
+extension Date {
+    var ticks: UInt64 {
+        return UInt64((self.timeIntervalSince1970 + 62_135_596_800) * 10_000_000)
+    }
 }
 
